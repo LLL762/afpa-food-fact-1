be0719e2519ca3e110c65e2fc4f9bf908dce5d9e
LLL762 <lamirallaurent@gmail.com>
2022-09-30 10:29:16 +0200

    search input validator fix+ more tests.

--- test/validation/search-input-validator.test.js
+++ test/validation/search-input-validator.test.js
@@ -1,8 +1,34 @@
 import { SearchInputValidator } from "../../src/validation/search-input-validator";
 import { describe, expect, test } from "@jest/globals";
 
-const testSubject = SearchInputValidator();
+const testSubject = SearchInputValidator(15);
 
-test("should return blank message when input is empty", () => {
-  expect(testSubject.validate("", 15)).toBe(testSubject.getBlankMsg());
+test("given valid string, validate should return empty string", () => {
+  expect(testSubject.validate("48656489774")).toBe("");
+});
+
+test("given tabb space string, validate should return blank message", () => {
+  expect(testSubject.validate("\t\t\t")).toBe(testSubject.getBlankMsg());
+});
+
+test("given empty string, validate should return blank message", () => {
+  expect(testSubject.validate("")).toBe(testSubject.getBlankMsg());
+});
+
+test("given blank string, validate should return blank message", () => {
+  expect(testSubject.validate("        ")).toBe(testSubject.getBlankMsg());
+});
+
+test("given undefined, validate should return blank message", () => {
+  expect(testSubject.validate(undefined)).toBe(testSubject.getBlankMsg());
+});
+
+test("given NaN string, validate should return NaN message", () => {
+  expect(testSubject.validate("un7 va89lid")).toBe(testSubject.getIsNanMsg());
+});
+
+test("given too long string, validate should return max char message", () => {
+  expect(testSubject.validate("9999999999999999999999999999999999999")).toBe(
+    testSubject.getMaxCharMsg()
+  );
 });
